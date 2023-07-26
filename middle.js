c2eb90dc52adf55d6e38afe0aee00848e1625193
= <steven.serruya@gmail.com>
2023-07-26 15:38:39 -0500

    addded the middle function

--- middle.js
+++ middle.js
@@ -17,5 +17,20 @@ const assertArraysEqual = function(actual, expected) {
   console.log(message);
 };
 
+const middle = function(arr) {
+  let halfOfIndex = arr.length / 2;
+  let returnedArray = [];
+
+  if (arr.length % 2 === 0) {
+    // Even-length array, push the middle two elements
+    returnedArray.push(arr[halfOfIndex - 1], arr[halfOfIndex]);
+  } else {
+    // Odd-length array, push the single middle element
+    let middleIndex = Math.floor(halfOfIndex);
+    returnedArray.push(arr[middleIndex]);
+  }
+
+  return returnedArray;
+};
 
 
