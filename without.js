80a3374dafafad8fcbd5ef55241c5fb394c7c18d
= <steven.serruya@gmail.com>
2023-07-25 20:17:43 -0500

    added the without function

--- without.js
+++ without.js
@@ -16,3 +16,14 @@ const assertArraysEqual = function(actual, expected) {
   const message = isEqual ? "✅ Arrays are equal." : "🛑 Arrays are NOT equal.";
   console.log(message);
 };
+
+const without = function(arr, itemToRemove) {
+  return arr.filter(item => !itemToRemove.includes(item));
+};
+
+
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
