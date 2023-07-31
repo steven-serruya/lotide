106fce3730f8624df868746298259962f9660ceb
= <steven.serruya@gmail.com>
2023-07-31 18:55:10 -0500

    refactored tail

--- tail.js
+++ tail.js
@@ -1,13 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
+const assertEqual = require('./assertEqual');
+
+
 const tail = function(arr) {
   return arr.slice(1);
 };
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
+module.exports = tail;
