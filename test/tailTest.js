96796d036378a0b5c7a5012db1be18752cd716bf
= <steven.serruya@gmail.com>
2023-07-31 21:02:52 -0500

    refactored middleTest

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,18 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
+// const assertEqual = require('../assertEqual');
+// const tail = require('../tail');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result, ["Lighthouse", "Labs"]);
+
+const assert = require('chai').assert;
+const { expect } = require('chai');
+const tail = require("../tail");
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    // Call the tail function with the array as an argument
+    const result = tail(['Hello', 'Lighthouse', 'Labs']);
+
+    // Compare the result with the expected array
+    expect(result).to.deep.equal(['Lighthouse', 'Labs']);
+  });
+});
